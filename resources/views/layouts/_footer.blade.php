aaa9f01c87ac69bf688aaa47da2e7c9c21a80527
donerzhang <626436761@qq.com>
2019-01-15 11:41:02 +0800

    基础页面结构

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+	<div class="container">
+		<p class="float-left">
+			由 Summer 设计和编码
+		</p>
+		
+		<p class="float-right"><a href="mailto:">联系我们</a></p>
+	</div>
+</footer>
